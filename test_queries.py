c0b1aeb1167b98af24c77024e6e06d19b0236bfb
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-01 10:51:38 -0500

    Added starter code for querying Blake's SQL database

---
+++ test_queries.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+connection = psycopg2.connect(
+    host = "localhost",
+    port = 5432,
+    database = "jonesb2",
+    user = "jonesb2",
+    password = "card254cup"
+)
+if connection is None:
+    print("Connection failed")
+cursor = connection.cursor()
