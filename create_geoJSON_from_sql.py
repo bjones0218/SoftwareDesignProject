f4949575b313ec6866fcbe98a32fc494b3d37da4
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-11 17:35:40 -0500

    Created a SQL command and added a bit of debugging info to test it

--- create_geoJSON_from_sql.py
+++ create_geoJSON_from_sql.py
@@ -17,5 +17,9 @@ data = {
     "features": []
 }
 
+cursor.execute("SELECT date, time, longitude, latitude, attack_type, location_description, nearest_country, eez_country, shore_distance, shore_longitude, shore_latitude, attack_description, vessel_name, vessel_type, vessel_status, data_source FROM pirate_attacks")
+all_attacks = cursor.fetchall()
+print(all_attacks[0])
+
 with open("pirate_attacks.geojson", "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
