7aa1cbb6a1018e1479d927c2976f0d57a81af24e
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-11 17:49:34 -0500

    Copying all of the data from SQL into a python dictionary, and then copying everything into JSON

--- create_geoJSON_from_sql.py
+++ create_geoJSON_from_sql.py
@@ -17,9 +17,34 @@ data = {
     "features": []
 }
 
+# put all of the data in the GeoJSON, because we don't know what we need yet
 cursor.execute("SELECT date, time, longitude, latitude, attack_type, location_description, nearest_country, eez_country, shore_distance, shore_longitude, shore_latitude, attack_description, vessel_name, vessel_type, vessel_status, data_source FROM pirate_attacks")
 all_attacks = cursor.fetchall()
 print(all_attacks[0])
+for attack in all_attacks:
+    longitude = attack[2]
+    latitude = attack[3]
+    # creating a point with the appropriate latitude and longitude
+    # https://en.wikipedia.org/wiki/GeoJSON
+    geoJSON_feature = {"type": "Feature", "geometry": {"type": "Point", "coordinates": [longitude, latitude]}}
+    # copying all extra data into the properties of the point
+    properties = dict(date = attack[0],
+                      time = attack[1],
+                      attack_type = attack[4],
+                      location_description = attack[5],
+                      nearest_country = attack[6],
+                      eez_country = attack[7],
+                      shore_distance = attack[8],
+                      shore_longitude = attack[9],
+                      shore_latitude = attack[10],
+                      attack_description = attack[11],
+                      vessel_name = attack[12],
+                      vessel_type = attack[13],
+                      vessel_status = attack[14],
+                      data_source = attack[15])
+    geoJSON_feature["properties"] = properties
+
+print(data["features"][0])
 
 with open("pirate_attacks.geojson", "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
