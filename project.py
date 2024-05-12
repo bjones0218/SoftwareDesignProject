d99f2d6e182d338ac725ab4e416f984b22c7541d
camsr003 <165401262+camsr003@users.noreply.github.com>
2024-05-12 14:28:21 -0500

    Update project.py

--- project.py
+++ project.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    message = "Welcome to the Home Webpage."
     return render_template("home.html")
     
 @app.route('/explore')
 def explore():
-    message = "This is the next page."
     return render_template("explore.html")
 if __name__ == '__main__':
     my_port = 5216
