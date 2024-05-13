1245ba04bcd332417fdf4a0d2c559904ba6ea54e
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-13 10:58:28 -0500

    Corrected file path

--- update_country_outlines_with_indicators.py
+++ update_country_outlines_with_indicators.py
@@ -20,7 +20,7 @@ data = {
 }
 
 # https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
-with open("raw_countries.geojson", "r", encoding="utf-8") as raw_country_file:
+with open("static/raw_countries.geojson", "r", encoding="utf-8") as raw_country_file:
     try:
         raw_data = json.load(raw_country_file)
     except JSONDecodeError:
