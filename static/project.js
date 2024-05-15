7e5dc8bf10e72df572016784432fc5b2b2d1ac7f
梁轩振 <liangl@carleton.edu>
2024-05-15 16:20:36 -0500

    headbar elements fixed

--- static/project.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    const headBar = document.getElementById('headBar');
-    if (window.pageYOffset > 100) {
-        headBar.style.backgroundColor = '#222';
-    } else {
-        headBar.style.backgroundColor = '#333';
-    }
-};
