5cd932add347c26a34d18d77259e80bf0e085d6d
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-15 20:50:51 -0500

    Labelling graph using year numbers

--- static/num_attacks_vs_time_chart.js
+++ static/num_attacks_vs_time_chart.js
@@ -1,10 +1,18 @@
 fetch('static/pirate_attacks.geojson').then((response) => response.json()).then((json) => console.log(json));
 
+const START_YEAR = 1993;
+const END_YEAR = 2020;
+
+years = [];
+for (var i=START_YEAR; i<=END_YEAR; i++) {
+    years.push(i);
+}
+
 const context = document.getElementById('attacks_vs_time_chart');
 
 https://www.chartjs.org/docs/latest/getting-started/
 new Chart(context, {type: 'bar',
-                data: {labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                data: {labels: years,
                        datasets: [{label: 'Number of attacks',
                                    data: [12, 19, 3, 5, 2, 3],
                                    borderWidth: 1}]},
