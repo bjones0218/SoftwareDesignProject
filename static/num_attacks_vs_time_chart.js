380d05bbee5f7095178e18ff98cb389ad6a53591
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-15 21:00:03 -0500

    Putting chart creation in an asynchronous function so it can go after getting the attacks

--- static/num_attacks_vs_time_chart.js
+++ static/num_attacks_vs_time_chart.js
@@ -1,10 +1,3 @@
-async function get_attacks() {
-    var attacks_response = await fetch('static/pirate_attacks.geojson');
-    var attacks = await attacks_response.json();
-    console.log(attacks);
-    return attacks;
-}
-
 const START_YEAR = 1993;
 const END_YEAR = 2020;
 
@@ -13,15 +6,26 @@ for (var i=START_YEAR; i<=END_YEAR; i++) {
     years.push(i);
 }
 
-get_attacks();
+async function get_attacks() {
+    var attacks_response = await fetch('static/pirate_attacks.geojson');
+    var attacks = await attacks_response.json();
+    console.log(attacks);
+    
+    return attacks;
+}
 
-const context = document.getElementById('attacks_vs_time_chart');
+async function create_chart() {
+    var attcks = await get_attacks();
+    const context = document.getElementById('attacks_vs_time_chart');
+    https://www.chartjs.org/docs/latest/getting-started/
+    new Chart(context, {type: 'bar',
+                    data: {labels: years,
+                        datasets: [{label: 'Number of attacks',
+                                    data: [12, 19, 3, 5, 2, 3],
+                                    borderWidth: 1}]},
+                    options: {scales: {y: {beginAtZero: true}}}});
+    console.log("Finished creating chart");
+}
 
-https://www.chartjs.org/docs/latest/getting-started/
-new Chart(context, {type: 'bar',
-                data: {labels: years,
-                       datasets: [{label: 'Number of attacks',
-                                   data: [12, 19, 3, 5, 2, 3],
-                                   borderWidth: 1}]},
-                options: {scales: {y: {beginAtZero: true}}}});
-console.log("Finished creating chart");
+create_chart();
+
