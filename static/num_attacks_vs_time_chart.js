ebdf86edd254a31225b32cd01b095444aba16bd9
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-15 20:47:08 -0500

    Getting pirate attack data from file

--- static/num_attacks_vs_time_chart.js
+++ static/num_attacks_vs_time_chart.js
@@ -1,9 +1,11 @@
+fetch('static/pirate_attacks.geojson').then((response) => response.json()).then((json) => console.log(json));
+
 const context = document.getElementById('attacks_vs_time_chart');
 
 https://www.chartjs.org/docs/latest/getting-started/
 new Chart(context, {type: 'bar',
                 data: {labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                       datasets: [{label: '# of Votes',
+                       datasets: [{label: 'Number of attacks',
                                    data: [12, 19, 3, 5, 2, 3],
                                    borderWidth: 1}]},
                 options: {scales: {y: {beginAtZero: true}}}});
