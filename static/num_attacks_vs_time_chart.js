6bdca939b4ff1fd0a3bcd5a4a131077938c43658
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-17 20:21:58 -0500

    Using a dictionary to get the region that each attack took place in. Creating separate, dynamically-generated maps for each region, only counting attacks happening in that region.

--- static/num_attacks_vs_time_chart.js
+++ static/num_attacks_vs_time_chart.js
@@ -13,8 +13,24 @@ async function get_attacks() {
     return attacks;
 }
 
-async function create_chart() {
+async function get_region_of_all_country_codes() {
+    var country_outlines_response = await fetch('static/country_outlines_with_indicators');
+    var country_outlines = await country_outlines_response.json();
+    var dictionary = {};
+    for (var i=0; i<country_outlines.features.length; i++) {
+        var country = country_outlines.features[i].properties;
+        dictionary[country.ISO_A3] = country.region;
+    }
+    return dictionary;
+}
+
+async function create_chart(region) {
     var attacks = await get_attacks();
+
+    var country_codes_lookup;
+    if (region != "World") {
+        country_codes_lookup = await get_region_of_all_country_codes();
+    }
 
     var attack_counts = Array(years.length);
     attack_counts.fill(0);
@@ -22,19 +38,26 @@ async function create_chart() {
     for (var i=0; i<attacks.features.length; i++) {
         var attack = attacks.features[i];
         var attack_year = parseInt(attack.properties.date.substring(0, 4));
-        attack_counts[attack_year - START_YEAR] += 1;
+        if (region == "World") {
+            attack_counts[attack_year - START_YEAR] += 1;
+        } else {
+            var attack_country_code = attack.properties.nearest_country;
+            var attack_region = country_codes_lookup[attack_country_code];
+            if (attack_region == region) {
+                attack_counts[attack_year - START_YEAR] += 1;
+            }
+        }
     }
 
     const context = document.getElementById('attacks_vs_time_chart');
+    var chart_label = region == "World" ? "Number of attacks" : "Number of attacks in " + region;
     https://www.chartjs.org/docs/latest/getting-started/
     new Chart(context, {type: 'line',
                     data: {labels: years,
-                        datasets: [{label: 'Number of attacks',
+                        datasets: [{label: chart_label,
                                     data: attack_counts,
                                     borderWidth: 1}]},
                     options: {scales: {y: {beginAtZero: true}},
                               fill: true}});
 }
-
-create_chart();
 
