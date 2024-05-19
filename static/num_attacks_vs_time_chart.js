be42c038396de21ed3c2712900525a46d5a70ced
梁轩振 <liangl@carleton.edu>
2024-05-18 20:03:56 -0500

    charts design updated

--- static/num_attacks_vs_time_chart.js
+++ static/num_attacks_vs_time_chart.js
@@ -64,8 +64,8 @@ async function create_chart(region) {
     //designing color
     const ctx = document.getElementById("attacks_vs_time_chart").getContext('2d');
     let gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, 'rgba(255, 255, 255, 0.1)');  // Lighter at the top
-    gradient.addColorStop(1, 'rgba(0, 0, 0, 0.85)');  
+    gradient.addColorStop(0, 'rgba(58, 123, 213, 1)');  // Lighter blue at the top
+    gradient.addColorStop(1, 'rgba(0, 31, 63, 1)'); 
 
     const context = document.getElementById('attacks_vs_time_chart');
     var chart_label = region == "World" ? "Number of attacks" : "Number of attacks in " + region;
