ed982b2672a1f6be8e7900397f9d9c507553a3e3
梁轩振 <liangl@carleton.edu>
2024-05-18 20:37:08 -0500

    charts design updated

--- static/num_attacks_vs_time_chart.js
+++ static/num_attacks_vs_time_chart.js
@@ -78,7 +78,7 @@ async function create_chart(region) {
                                     borderWidth: 1}]},
                     options: {scales: {y: {beginAtZero: true}},
                               fill: true,
-                              radius: 4,
+                              radius: 3.6,
                               hoverRadius:10,
                               responsive: true,
                               backgroundColor: gradient,
