57289c4c36def74ef935f303f789590a5256cc70
bjones0218 <blake28jones@gmail.com>
2024-05-20 12:12:48 -0500

    timing added

--- static/pirategame.js
+++ static/pirategame.js
@@ -10,13 +10,16 @@ var gameOver = false;
 var speedY = 0;
 var speedX = 0;
 var acceleration = 10;
-var positionY = 4*blockSize;
-var positionX = width*blockSize*0.25;
+var positionY = (height*blockSize)/2;
+var positionX = (width*blockSize)/2;
 
 var counter = 0;
 var ships = [];
 var myInterval;
-var myTimer;
+
+let minute = 0; 
+let second = 0; 
+let count = 0;
 
 window.onload = function () {
     // Set board height and width
@@ -26,6 +29,8 @@ window.onload = function () {
     context = board.getContext("2d");
     document.addEventListener("keyup", changeDirection);
     myInterval = setInterval(update, 1000 / frames);
+    timer = true; 
+    stopWatch();
 }
 
 function onStartClicked () {
@@ -37,10 +42,18 @@ function onStartClicked () {
         startButton.innerText = "Start";
         counter = 0;
         ships = [];
+        timer = false; 
+        minute = 0; 
+        second = 0; 
+        count = 0; 
+        document.getElementById('min').innerHTML = "00"; 
+        document.getElementById('sec').innerHTML = "00"; 
     }
     else {
         gameOver = false;
         startButton.innerText = "Restart";
+        timer = true;
+        stopWatch();
         myInterval = setInterval(update, 1000 / frames);
     }
 
@@ -54,7 +67,7 @@ function checkCollision (ship){
 		((positionY >= ((ship[0] + blockSize) - 2)) && (positionY <= ((ship[0] + blockSize) + 2 )) && (positionX + blockSize >= ship[1]) && ((positionX + blockSize) <= ship[1] + (2 * blockSize))) ||
         (positionY + blockSize >= (ship[0] - 2)) && (positionY + blockSize <= (ship[0]+ 2 )) && (positionX + blockSize >= ship[1]) && ((positionX + blockSize) <= ship[1] + (2 * blockSize)) ){ 
 		
-            console.log("collision occurred");
+        console.log("collision occurred");
 		return true;
 	}
 	return false;
@@ -92,6 +105,7 @@ function update (){
 		if (collision){
 			gameOver = true;
             alert("Game Over");
+            timer = false;
 		}
 
         ships[i][0] = ships[i][0] + 1;
@@ -109,6 +123,7 @@ function update (){
     if ((positionY >= height * blockSize) || (positionX >= width * blockSize) || (positionY < 0) || (positionX < 0) ){
         alert("Game Over");
         gameOver = true;
+        timer = false;
     }
 
 }
@@ -136,4 +151,37 @@ function changeDirection(e) {
 function addShip (){
     newShip = Math.floor(Math.random() * (width));
     ships.push([-20, newShip*blockSize])
+}
+
+function stopWatch() { 
+    if (timer) { 
+        count++; 
+  
+        if (count == 100) { 
+            second++; 
+            count = 0; 
+        } 
+  
+        if (second == 60) { 
+            minute++; 
+            second = 0; 
+        } 
+
+        let minString = minute; 
+        let secString = second; 
+  
+        if (minute < 10) { 
+            minString = "0" + minString; 
+        } 
+  
+        if (second < 10) { 
+            secString = "0" + secString; 
+        } 
+  
+
+        document.getElementById('min').innerHTML = minString; 
+        document.getElementById('sec').innerHTML = secString; 
+
+        setTimeout(stopWatch, 10); 
+    } 
 }
