e27546c6ef8016e9b4fd4b7922429bd152478d7f
梁轩振 <liangl@carleton.edu>
2024-05-27 10:52:25 -0500

    headbar map added

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
     
-@app.route('/explore')
+@app.route('/map')
 def explore():
     return render_template("explore.html")
 
