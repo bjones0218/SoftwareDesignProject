4e50e79e426c422fbe8e8d7c7fb34169bd7c7ad4
梁轩振 <liangl@carleton.edu>
2024-05-28 13:21:39 -0500

    hover effect fixed

--- static/project.js
+++ static/project.js
@@ -6,6 +6,9 @@ function mouseEnterExplore(){
     bigexplore.style.backgroundColor = "#CD853F";
     bigexplore.style.transform = "scale(1.2)";
     bigexplore.style.transition = "all .2s ease-in"
+    mapbutton = document.getElementById("map");
+    mapbutton.style.backgroundColor = "#rgb(78, 74, 74)";
+    mapbutton.style.color = "white";
 };
 
 //this function removes the hover effect when teh mouse leave the button
@@ -14,4 +17,7 @@ function mouseOutExplore(){
     bigexplore.style.backgroundColor = "#D2B48C";
     bigexplore.style.transform = "scale(1)";
     bigexplore.style.transition = "all .2s ease-out"
+    mapbutton = document.getElementById("map");
+    mapbutton.style.backgroundColor = "#555";
+    mapbutton.style.color = "white";
 };
